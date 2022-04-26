ef45b6c0de94bd01dd0f3b9d9f1e17fa652d4156
Luciano_Freitas <64561258+Ludoug-f@users.noreply.github.com>
2022-04-26 15:46:57 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+import { getCategories } from './services/api';
 
 function App() {
   return (
     <div>
-      {console.log(getCategories(),
-        getProductsFromCategoryAndQuery('MLB5672', 'Acessórios para Veículos'))}
+      {console.log(getCategories())}
     </div>
   );
 }
